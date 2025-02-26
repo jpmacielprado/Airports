98e52821e23d16422ee4d45aef7b3412bdd96553
jpmacielprado <jp.macielprado@gmail.com>
2025-02-26 10:43:55 -0300

    Término da fase 3 >> - Implementado EndPoint FindByCityIgnoreCase >> - Update AirportRepository - Adicionado assinatura de método >> FindByCityIgnoreCase; >> >> - Update AirportService - Adicionado método FindByCity >> - Update AirportController - Adicionado EndPoint /city/{cityName}

--- src/main/java/local/jotape/airports/controllers/AirportController.java
+++ src/main/java/local/jotape/airports/controllers/AirportController.java
@@ -4,7 +4,9 @@ import local.jotape.airports.service.AirportService;
 import java.util.List;
 import local.jotape.airports.entities.Airport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,4 +26,23 @@ public class AirportController {
         return result;
     }
     
+    /**EndPoint /airports/city/{city name}
+     * @param cityName
+     * @return
+     */
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<List<Airport>> finByCityIgnoreCase(@PathVariable String cityName) {
+        List<Airport> result = airportService.findByCity(cityName);
+        
+        if (result.isEmpty()) {
+            // ops... lista vazia
+            // not found devolve 404
+            return ResponseEntity.notFound().build();
+        } else {
+            //boa! tem dados
+            //devolvo ok 202
+            return ResponseEntity.ok(result);
+            
+        }
+    }
 }
--- src/main/java/local/jotape/airports/repositories/AirportRepository.java
+++ src/main/java/local/jotape/airports/repositories/AirportRepository.java
@@ -1,6 +1,7 @@
 
 package local.jotape.airports.repositories;
 
+import java.util.List;
 import local.jotape.airports.entities.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author sesideva
  */
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+    
+    List<Airport> findByCityIgnoreCase (String city);
     
 }
